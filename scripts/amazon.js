62e26485ea9a45b5b0c4814a67928e29821ae3cd
nikkanap <nenaputo@up.edu.ph>
2025-09-11 08:50:13 +0800

    Use modules and import cart and products from cart.js and products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,8 @@
+//import { cart as myCart} from '../data/cart.js';
+import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
+//const cart = [];
 
 let productsHTML = '';
 products.forEach((product) => {
