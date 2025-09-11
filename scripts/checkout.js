0e106b75219dad2e983b42ef64387b144c2f16d7
nikkanap <nenaputo@up.edu.ph>
2025-09-11 09:52:28 +0800

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,5 +101,7 @@ document.querySelectorAll('.js-delete-link')
         removeFromCart(productId);
         console.log(cart);
 
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
